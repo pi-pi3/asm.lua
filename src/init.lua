1cda4bfc208c79cea2646fa5685ccf0fb3d74457
Szymon Walter <walter.szymon.98@gmail.com>
2017-09-08 23:06:47 +0200

    Add compiler skeleton

--- src/init.lua
+++ src/init.lua
@@ -1,26 +1,105 @@
 
-boilerplate = [[
+local boilerplate = [[
 _R={a=0,b=0,c=0,d=0,ss='\0',ds='\0',f={gt=false,lt=false,ge=false,le=false,eq=false,ne=false,err=false,syserr=false},_R.sp=1}
 _X={}
 _D={}
 ]]
 
--- section .NAME => {type = 'section', name = 'NAME'}
--- NAME: => {type = 'label', name = 'NAME'}
--- OP DST, SRC => {type = 'op', name = 'OP', dst = 'DST', src = 'SRC'}
--- end => {type = 'end'}
--- if COND then => {type = 'if', cond = 'COND'}
--- while COND do => {type = 'while', cond = 'COND'}
--- do => {type = 'do'}
--- until COND => {type = 'while', cond = 'COND'}
--- loop => {type = 'loop'}
-function genast(src)
-    -- TODO match string match
-    return {}
+local ex = {}
+-- TODO: improve readability
+ex.doloop = {pattern = 'do', type = 'do', out = {}}
+ex.endb = {pattern = 'end', type = 'end', out = {}}
+ex.loop = {pattern = 'loop', type = 'loop', out = {}}
+ex.label = {pattern = '(%a+)%s*:', type = 'label', out = {'name'}}
+ex.section = {pattern = 'section .(%a+)', type = 'section', out = {'name'}}
+ex.ifthen = {pattern = 'if (%a+) then', type = 'if', out = {'cond'}} -- TODO: cond validation
+ex.whiledo = {pattern = 'while (%a+) do', type = 'while', out = {'cond'}}
+ex.untilloop = {pattern = 'until (%a+)', type = 'until', out = {'cond'}}
+ex.op2 = {pattern = '(%a+) ([%a%d%[%]]+), ([%a%d%[%]]+)', type = 'op', out = {'op', 'a', 'b'}}
+ex.op1 = {pattern = '(%a+) ([%a%d%[%]]+)', type = 'op', out = {'op', 'a'}}
+ex.op0 = {pattern = '(%a+)', type = 'op', out = {'op'}}
+ex.extern = {pattern = 'extern ([%a%d_]+)', type = 'extern', out = {'name'}}
+ex.defstr = {pattern = 'def ([%a%d_]+) (".*")', type = 'def', out = {'name', 'val'}} -- TODO: string validation
+ex.defnum = {pattern = 'def ([%a%d_]+) (0?[xb]?[%da-fA-F]?%.?[%da-fA-F]+)', type = 'def', out = {'name', 'val'}}
+
+for _, v in pairs(ex) do
+    local b = '^%s*' -- begin
+    local e = '%s*$' -- end
+    local s = '%s+'  -- space one or more
+    v.pattern = b .. string.gsub(v.pattern, ' ', '\\s+') .. e
 end
 
+function match(expr, ex, result)
+    local out = {string.match(expr, ex.pattern)}
+    if #out == 0 then
+        return false
+    end
+
+    result.type = ex.type
+
+    for i = 1, #out do
+        local name = ex.out[i]
+        result[name] = out[i]
+    end
+
+    return true
+end
+
+function genast(src)
+    local line = 1
+    local ast = {}
+
+    for expr in string.gmatch(src, '.') do
+        expr = string.gsub(expr, '%s*%-%-.*$', '')
+        -- TODO check for invalid characters 
+        if not string.match(expr, '^%s*$') then
+            for _, v in pairs(ex) do
+                local result = {}
+                if match(expr, v, result) then
+                    ast[#ast+1] = result
+                else
+                    error(string.format('invalid expression: %s' ..
+                                        '           at line: %d', expr, line))
+                end
+            end
+        end
+        line = line + 1
+    end
+
+    return ast
+end
+
+comp = {}
+comp['do'] = {pattern = 'do', arg = {}}
+comp['end'] = {pattern = 'end', arg = {}}
+comp['loop'] = {pattern = '_R.c=_R.c+1;while _R.c>1 do _R.c=_R.c-1', arg = {}}
+comp['label'] = {pattern = 'function %s()', arg = {'name'}}
+comp['if'] = {pattern = 'if _R.f.%s then', arg = {'cond'}}
+comp['while'] = {pattern = 'while _R.f.%s do', arg = {'cond'}}
+comp['until'] = {pattern = 'until _R.f.%s', arg = {'cond'}}
+comp['extern'] = {pattern = '_X["%s"] = %s', arg = {'name'}}
+
+local section = 'text'
 function expr_to_lua(expr)
-    return "\n"
+    local type = expr.type
+
+    if type == 'section' then
+        section = expr.name
+        return ''
+    elseif type == 'op' then -- TODO
+        return ''
+    elseif type == 'def' then -- TODO
+        return ''
+    else
+        local args = {}
+        for k, _ in pairs(comp[type].arg) do
+            comp[type].arg[k] = expr[k]
+        end
+
+        local lua
+        lua = string.format(comp[type].pattern, table.unpack(args))
+        return lua
+    end
 end
 
 function ast_to_lua(ast)
@@ -33,7 +112,16 @@ end
 
 function compile(src)
     local dst = boilerplate
-    local ast = genast(src)
+
+    local ast
+    local status, result = genast(src)
+    if status then
+        ast = result
+    else
+        print(result)
+        os.exit(1)
+    end
+
     dst = dst .. ast_to_lua(ast)
     return dst
 end
