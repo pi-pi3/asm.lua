bb7ad50ac616afd7754b0a972ebbd2c604e70550
Szymon Walter <walter.szymon.98@gmail.com>
2017-09-10 17:23:41 +0200

    Fix patterns

--- include/parseline.lua
+++ include/parseline.lua
@@ -3,17 +3,17 @@ local ex = {
     {type = 'do', pattern = 'do', out = {}},
     {type = 'end', pattern = 'end', out = {}},
     {type = 'loop', pattern = 'loop', out = {}},
-    {type = 'label', pattern = '(%a+)%s*:', out = {'name'}},
-    {type = 'section', pattern = 'section %.(%a+)', out = {'name'}},
+    {type = 'label', pattern = '([%a%d_%-]+)%s*:', out = {'name'}},
+    {type = 'section', pattern = 'section %.([%a%d_%-]+)', out = {'name'}},
     {type = 'if', pattern = 'if (%a+) then', out = {'cond'}}, -- TODO: cond validation
     {type = 'while', pattern = 'while (%a+) do', out = {'cond'}},
     {type = 'until', pattern = 'until (%a+)', out = {'cond'}},
     {type = 'extern', pattern = 'extern ([%a%d_]+)', out = {'name'}},
-    {type = 'def', pattern = 'def ([%a%d_]+) (".*")', out = {'name', 'val'}}, -- TODO: string validation
-    {type = 'def', pattern = 'def ([%a%d_]+) (0x[%da-fA-F]*%.?[%da-fA-F]+)', out = {'name', 'val'}},
-    {type = 'def', pattern = 'def ([%a%d_]+) (0[0-7]*%.?[0-7]+)', out = {'name', 'val'}},
-    {type = 'def', pattern = 'def ([%a%d_]+) (0b[01]*%.?[01]+)', out = {'name', 'val'}},
-    {type = 'def', pattern = 'def ([%a%d_]+) (%d*%.?%d+)', out = {'name', 'val'}},
+    {type = 'def', pattern = 'def ([%a%d_%-]+) (".*")', out = {'name', 'val'}}, -- TODO: string validation
+    {type = 'def', pattern = 'def ([%a%d_%-]+) (0x[%da-fA-F]*%.?[%da-fA-F]+)', out = {'name', 'val'}},
+    {type = 'def', pattern = 'def ([%a%d_%-]+) (0[0-7]*%.?[0-7]+)', out = {'name', 'val'}},
+    {type = 'def', pattern = 'def ([%a%d_%-]+) (0b[01]*%.?[01]+)', out = {'name', 'val'}},
+    {type = 'def', pattern = 'def ([%a%d_%-]+) (%d*%.?%d+)', out = {'name', 'val'}},
     {type = 'op', pattern = '(%a+) ([%a%d%[%]_%-]+), ([%a%d%[%]_%-]+)', out = {'op', 'a', 'b'}},
     {type = 'op', pattern = '(%a+) ([%a%d%[%]_%-]+)', out = {'op', 'a'}},
     {type = 'op', pattern = '(%a+)', out = {'op'}}
